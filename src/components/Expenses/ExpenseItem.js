3683780c7aac86bc8d44db7b74503a8f84245b10
Wan Chang Lai <wanchang.lai@live.com>
2022-10-29 22:00:42 +0800

    Add ExpenseForm Without Event Listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,11 +8,13 @@ function ExpenseItem(props) {
   //title is the thing we want to change//
   //settitle is the thing we change to //
   const [title,setTitle] = useState(props.title);
+  //still get the title for the first time of code running//
+  //state will run the code twice, can validate the data by plugin log it//
+  //title will change it second time//
 
   const clickHandler = () =>{
-    setTitle('Updated');
-    //still get the title for the first time of code running//
-    //change it second time//
+  //we do not use the title="" this will cause error//
+    setTitle('Updated');   
     console.log(title);
   };
 
