1a72e81426926f80d816fe33d0f259f79f07f2b7
Wan Chang Lai <wanchang.lai@live.com>
2022-11-07 18:58:22 +0800

    Add Cancel Button

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,41 +7,16 @@ const ExpenseForm = (props) =>{
   const [enteredAmount,setEnteredAmount]=useState("");
   const [enteredDate,setEnteredDate]=useState("");
 
-//   const [userInput,setUserInput] = useState({
-//     enteredTitle:"",
-//     enteredAmount:"",
-//     enteredDate:""
-//   });
-
   const titleChangeHandler = (event) =>{
     setEnteredTitle(event.target.value);
-    // setUserInput({
-    //     //...userInput remains all thing in userInput but overwrite enteredTitle//
-    //     ...userInput,
-    //     enteredTitle:event.target.value, 
-    // });
-    // setUserInput((prevState)=>{
-    //     return {...prevState,enteredTitle:event.target.value};
-    // });
   };
 
   const amountChangeHandler = (event) =>{
     setEnteredAmount(event.target.value);
-    // setUserInput({
-    //     ...userInput,
-    //     enteredAmount:event.target.value, 
-    // })
-    // setUserInput((prevState)=>{
-    //     return{...prevState,enteredAmount:event.target.amount};
-    // })
   };
 
   const dateChangeHandler = (event) =>{
     setEnteredDate(event.target.value)
-    // setUserInput({
-    //     ...userInput,
-    //     enteredDate:event.target.value, 
-    // })
   };
 
   const submitHandler = (event) =>{
@@ -75,9 +50,10 @@ const ExpenseForm = (props) =>{
             <div className="new-expense__control">
                 <label>Date</label>
                 <input type="date" min="2019-01-01" max="2025-12-31" value={enteredDate} onChange={dateChangeHandler}/>
-             </div>
+            </div>
         </div>
         <div className="new-expense__actions">
+            <button type="button" onClick={props.onCancel}>Cancel</button>
             <button type="submit">Add Expense</button>
         </div>
      </form>
