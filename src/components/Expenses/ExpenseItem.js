f513edb6e4a86cb0392c09118018ef3f892621ed
Wan Chang Lai <wanchang.lai@live.com>
2022-11-07 14:08:03 +0800

    Apply Conditional Content

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,32 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
-  //title is the thing we want to change//
-  //settitle is the thing we change to //
-  const [title,setTitle] = useState(props.title);
-  //still get the title for the first time of code running//
-  //state will run the code twice, can validate the data by plugin log it//
-  //title will change it second time//
-
-  const clickHandler = () =>{
-  //we do not use the title="" this will cause error//
-    setTitle('Updated');   
-    console.log(title);
-  };
-
   return (
-    <Card className='expense-item'>
+    <li>
+      <Card className='expense-item'>
         <ExpenseDate date ={props.date} />    
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+      </li>   
   );
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
 import React, { useState }from 'react';
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css'
+
+
 
 function Expenses(props){
 
@@ -17,21 +19,17 @@ function Expenses(props){
   });
 
   return (
-    <div>
-         
+    <div>        
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title} 
-            amount={expense.amount} 
-            date={expense.date}
-          />
-          ))}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
 );
+
+
+  
 };
+
 
 export default Expenses;
